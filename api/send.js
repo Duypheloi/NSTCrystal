8609d1e0b246ce08fa970b734e530d6aea5b4919
Duypheloi <nstcrystal@gmail.com>
2025-11-30 13:26:58 +0700

    add link api vercel key: WEBHOOK_URL

--- api/send.js
+++ api/send.js
@@ -3,10 +3,10 @@ export default async function handler(req, res) {
         return res.status(405).json({ error: "Only POST allowed" });
     }
 
+    const WEBHOOK = process.env.WEBHOOK_URL;
     const { name, question } = req.body;
     const time = new Date().toLocaleString("vi-VN");
 
-    const WEBHOOK = process.env.WEBHOOK_URL;
 
     const payload = {
         content:
@@ -24,3 +24,22 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ success: true });
 }
+// export default async function handler(req, res) {
+//   if (req.method !== "POST") {
+//     return res.status(405).json({ error: "Method not allowed" });
+//   }
+
+//   const webhookURL = process.env.WEBHOOK_URL;
+
+//   try {
+//     await fetch(webhookURL, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(req.body)
+//     });
+
+//     res.status(200).json({ success: true });
+//   } catch (err) {
+//     res.status(500).json({ error: "Failed to send message" });
+//   }
+// }
