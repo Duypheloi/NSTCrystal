c4a6e54d32143c25271a7f95413ef71b1459afd1
Duypheloi <nstcrystal@gmail.com>
2025-11-30 14:21:28 +0700

    fix api

--- api/send.js
+++ api/send.js
@@ -1,7 +1,11 @@
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://nstcrystal.is-a.dev/");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
 
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Why is it wrong again?" });
@@ -9,10 +13,10 @@ export default async function handler(req, res) {
 
     const WEBHOOK = process.env.WEBHOOK_URL;
     const SECRET = process.env.API_SECRET;
-    const { name, question } = req.body;
+    const { name, question, birthday } = req.body;
     const time = new Date().toLocaleString("vi-VN");
 
-    if (req.body.birthday !== SECRET) {
+    if (birthday !== SECRET) {
         return res.status(401).json({ error: "Unauthorized" });
     }
 
